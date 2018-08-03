8284bd100262c1254432200b0eb3c656c7c9275c
bcornille <bcornille7@gmail.com>
2018-08-03 11:31:22 -0500

    Added double pointer matmul.

--- test/perf/matmult.cpp
+++ test/perf/matmult.cpp
@@ -37,6 +37,56 @@ static void pointer(benchmark::State& state)
 }
 BENCHMARK(pointer)->Range(8, 1<<8);
 
+static void double_pointer(benchmark::State& state)
+{
+	for(auto _ : state) {
+		int N = state.range(0);
+		double** mat_a = new double*[N];
+		double** mat_b = new double*[N];
+		double** mat_c = new double*[N];
+
+		for (int i = 0; i < N; ++i)
+		{
+			mat_a[i] = new double[N];
+			mat_b[i] = new double[N];
+			mat_c[i] = new double[N];
+		}
+
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{
+				mat_a[i][j] = i;
+				mat_b[i][j] = j;
+				mat_c[i][j] = 0.0;
+			}
+		}
+
+		for (int i = 0; i < N; ++i)
+		{
+			for (int k = 0; k < N; ++k)
+			{
+				for (int j = 0; j < N; ++j)
+				{
+					mat_c[i][j] += mat_a[i][k]*mat_b[k][j];
+				}
+			}
+		}
+
+		for (int i = 0; i < N; ++i)
+		{
+			delete[] mat_a[i];
+			delete[] mat_b[i];
+			delete[] mat_c[i];
+		}
+
+		delete[] mat_a;
+		delete[] mat_b;
+		delete[] mat_c;
+	}
+}
+BENCHMARK(double_pointer)->Range(8, 1<<8);
+
 static void bracket_operator(benchmark::State& state)
 {
 	for(auto _ : state) {
